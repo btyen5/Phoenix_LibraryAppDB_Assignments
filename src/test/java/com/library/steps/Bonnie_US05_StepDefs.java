9572be38ea982b56abc578223f4d4ae6cbea01d7
btyen5 <btyen5@gmail.com>
2023-08-07 19:05:52 -0700

    updated library.sql and config.properties

---
+++ src/test/java/com/library/steps/Bonnie_US05_StepDefs.java
@@ -0,0 +1,16 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Bonnie_US05_StepDefs {
+
+    @When("I execute query to find most popular book genre")
+    public void i_execute_query_to_find_most_popular_book_genre() {
+
+    }
+    @Then("verify {string} is the most popular book genre.")
+    public void verify_is_the_most_popular_book_genre(String string) {
+
+    }
+}
--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -1,3 +1,34 @@
+-- US01
 select count(id) from users; -- actual
 
 select count(distinct id) from users; -- expected
+
+-- US02
+select count(*) from book_borrow where is_returned=0;
+
+-- US03
+select name from book_categories;
+
+-- US04
+select * from books where name='Clean Code';
+
+-- US05
+select bc.name,count(*) from book_borrow bb
+inner join books b on bb.book_id = b.id
+inner join book_categories bc on b.book_category_id=bc.id
+group by name
+order by 2 desc;
+
+-- US06
+select id,name,author from books
+where name = 'Clean Code' and author='Robert C.Martin'
+order by id desc;
+
+-- US07
+select full_name,b.name,bb.borrowed_date from users u
+inner
+inner
+join book_borrow bb on u.id = bb.user_id
+join books b on bb.book_id = b.id
+where full_name='Test Student 1’ and name=‘Head First Java'
+order by 3 desc;
