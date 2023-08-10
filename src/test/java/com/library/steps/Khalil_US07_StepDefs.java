bf457e3404fadd584f4015639446af5a7a856400
khali <khalil.elkhoja@hotmail.co.uk>
2023-08-10 16:49:03 -0700

    Created Feature file and StepDefs class, and added the snippets.

---
+++ src/test/java/com/library/steps/Khalil_US07_StepDefs.java
@@ -0,0 +1,29 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Khalil_US07_StepDefs {
+
+    @Given("the user searches for {string} book")
+    public void the_user_searches_for_book(String string) {
+
+    }
+
+    @When("the user clicks Borrow Book")
+    public void the_user_clicks_borrow_book() {
+
+    }
+
+    @Then("verify that book is shown in {string} page")
+    public void verify_that_book_is_shown_in_page(String string) {
+
+    }
+
+    @Then("verify logged student has same book in database")
+    public void verify_logged_student_has_same_book_in_database() {
+
+    }
+
+}
