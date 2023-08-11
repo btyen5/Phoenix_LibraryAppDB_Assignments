ac65799e9a875b741d4155362e2f0de8096aaa37
Senai <simoseni@gmail.com>
2023-08-11 09:18:09 -0700

    Step Definition created

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -14,8 +14,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
-        dryRun = false,
-        tags = "@wip"
+        dryRun = true,
+        tags = "@senai"
 )
 public class CukesRunner {
 
---
+++ src/test/java/com/library/steps/Senai03_Step_Defs.java
@@ -0,0 +1,17 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Senai03_Step_Defs {
+
+    @When("the user clicks book categories")
+    public void the_user_clicks_book_categories() {
+
+    }
+    @Then("verify book categories must match book_categories table from db")
+    public void verify_book_categories_must_match_book_categories_table_from_db() {
+
+    }
+
+}
